87a71364f3f8f3e7b099204a5680fcc8f069d36c
Erik Westrup <erik.westrup@gmail.com>
2022-02-11 11:17:49 +0100

    coproc example

--- bash/capture_output.sh
+++ bash/capture_output.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Capture output of command to variable.
 _capture() {
 	declare -n refvar="$1"
 	shift
@@ -14,5 +15,30 @@ _capture output \
 	"arg1" \
 	"arg2"
 
-echo "ecode: $?"
-echo "captured: $output"
+echo -e "> ecode: $?"
+echo "> captured: $output"
+
+
+
+
+
+printf "==========\n"
+
+# Runs command in background using coproc.
+# Reference: https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Coprocesses
+_capture_coproc() {
+	declare -n refvar="$1"
+	shift
+	# Quote $@ to preserve quoted strings. Ref. https://stackoverflow.com/a/39463371/265508
+	local cmd="${@@Q}"
+	coproc CMDPROC (eval "$cmd" | tee /dev/tty)
+	read -r refvar <&"${CMDPROC[0]}"
+}
+
+_capture output \
+	printf "test %s, %s\n" \
+	"arg1" \
+	"arg2"
+
+echo -e "\n> ecode: $?"
+echo "> captured: $output"
