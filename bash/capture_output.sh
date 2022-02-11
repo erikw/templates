9ee0f7063e86aa0050bb77c5146f4e3526152bcf
Erik Westrup <erik.westrup@gmail.com>
2022-02-11 11:09:15 +0100

    Indent with proper tabs

--- bash/capture_output.sh
+++ bash/capture_output.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 _capture() {
-  declare -n refvar="$1"
-  shift
-  # Quote $@ to preserve quoted strings. Ref. https://stackoverflow.com/a/39463371/265508
-  local cmd="${@@Q}"
-  # Preserves exit code from $cmd.
-  refvar=$((eval "$cmd" | tee /dev/tty; exit ${PIPESTATUS[0]})&)
+	declare -n refvar="$1"
+	shift
+	# Quote $@ to preserve quoted strings. Ref. https://stackoverflow.com/a/39463371/265508
+	local cmd="${@@Q}"
+	# Preserves exit code from $cmd.
+	refvar=$((eval "$cmd" | tee /dev/tty; exit ${PIPESTATUS[0]})&)
 }
 
 _capture output \
